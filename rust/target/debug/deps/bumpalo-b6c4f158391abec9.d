caa28819561b2d4770cdb962d882bb8fb8810cdf
Kinleoapple <996711203@qq.com>
2026-07-29 22:27:09 +0800

    chore(rust): add .gitignore, remove target from tracking

--- rust/target/debug/deps/bumpalo-b6c4f158391abec9.d
+++
@@ -1,9 +0,0 @@
-/Users/haojunhuang/CLionProjects/Glue/rust/target/debug/deps/bumpalo-b6c4f158391abec9.d: /Users/haojunhuang/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/lib.rs /Users/haojunhuang/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/alloc.rs /Users/haojunhuang/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/../README.md
-
-/Users/haojunhuang/CLionProjects/Glue/rust/target/debug/deps/libbumpalo-b6c4f158391abec9.rlib: /Users/haojunhuang/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/lib.rs /Users/haojunhuang/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/alloc.rs /Users/haojunhuang/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/../README.md
-
-/Users/haojunhuang/CLionProjects/Glue/rust/target/debug/deps/libbumpalo-b6c4f158391abec9.rmeta: /Users/haojunhuang/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/lib.rs /Users/haojunhuang/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/alloc.rs /Users/haojunhuang/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/../README.md
-
-/Users/haojunhuang/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/lib.rs:
-/Users/haojunhuang/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/alloc.rs:
-/Users/haojunhuang/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/../README.md:
